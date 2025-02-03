cb90bf27007f7b8ba9e7d9b40017a512a8e5b0ab
xfsd1448-uol <xfsd1448@leeds.ac.uk>
2025-02-03 13:35:36 +0000

    .

--- ordered_list.py
+++ ordered_list.py
@@ -1,1 +1,5 @@
-
+def get_orderd_list(lst=none):
+    if lst = None:
+        input_str = input("Enter numbers separated by commas: ")
+        lst = [int(x.strip()) for x in user_input.split(",")]
+    return sort.lst()
